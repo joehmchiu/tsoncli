2af395daedab42d8077b70587e8d4b8836f112f0
Your Name <you@example.com>
2020-09-24 13:35:34 +1000

    update

--- setup.py
+++ setup.py
@@ -1,22 +1,24 @@
 from setuptools import setup
 
-# python setup.py bdist_wheel
 # sudo rm -rf build/ dist/ tsoncli.egg-info/ /__pycache__/
+# python setup.py bdist_wheel
+# python3 -m twine upload dist/*
 def readme():
     with open('README.md') as f:
         return f.read()
 
 setup(name='tsoncli',
-      version='0.2',
+      version='0.3',
       description='TSON Command Line Interface',
       long_description=readme(),
       scripts=['bin/tson-cli'],
       classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3',
         'Topic :: Text Processing :: Linguistic',
       ],
+      python_requires='>=3.6',
       keywords='TSON tson cli',
       url='http://github.com/joehmchiu/tsoncli',
       author='Joe Chiu',
